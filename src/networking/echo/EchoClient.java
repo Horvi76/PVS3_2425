8dc7c0e6c564f7a710e37ad59749a5c4b61e976e
bohac <honza.boh00@gmail.com>
2026-01-14 12:25:20 +0100

    Echo client

---
+++ src/networking/echo/EchoClient.java
@@ -0,0 +1,50 @@
+package networking.echo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+
+    public static void main(String[] args) {
+        int port = 11111;
+        String host = "127.0.0.1";
+
+        System.out.println("Pripojuji se k " + host + ":" + port);
+
+        try(Socket socket = new Socket(host, port)) {
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            System.out.println("Pripojeno k serveru");
+
+            Scanner sc = new Scanner(System.in);
+
+            while (true){
+                System.out.println("Zadej zpravu, kterou chces poslat (quit pro ukonceni): ");
+
+                String message = sc.nextLine();
+                if (message.equalsIgnoreCase("quit")){
+                    break;
+                }
+
+                //zpravu ze scanneru (klavesnice/konzole) nacti a posli do outputu -> server
+                pw.println(message);
+
+                String response = in.readLine();
+
+                if (response == null){
+                    System.out.println("Server ukoncil pripojeni");
+                    break;
+                }
+                System.out.println("Echo od serveru: " + response);
+            }
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
