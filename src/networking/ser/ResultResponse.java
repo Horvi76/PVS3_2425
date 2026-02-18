762db043022384820d4f83e82ae34271390402f9
bohac <honza.boh00@gmail.com>
2026-02-18 13:00:27 +0100

    Zadani

---
+++ src/networking/ser/ResultResponse.java
@@ -0,0 +1,17 @@
+package networking.ser;
+
+import java.io.Serializable;
+
+public class ResultResponse implements Serializable {
+    String text;
+    ResultType type;
+
+    void logic(){
+        if (type == ResultType.OK){
+            text = new Result(0,"B").toString();
+        } else {
+            text = "ERoRR: bad numbers, illegal input....";
+        }
+    }
+
+}
---
+++ src/networking/ser/ResultType.java
@@ -0,0 +1,8 @@
+package networking.ser;
+
+import java.io.Serializable;
+
+public enum ResultType implements Serializable {
+    OK,
+    ERROR
+}
